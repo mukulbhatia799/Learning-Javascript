c6454d6093d2c93371cd3781881e0fc2a028c2bd
Mukul Bhatia <mukulbhatia189@gmail.com>
2024-03-05 00:55:26 +0530

    first commit

--- 3_pigGame/script.js
+++ 3_pigGame/script.js
@@ -3,7 +3,19 @@
 let player1CurrentScore = 0, player2CurrentScore = 0;
 let player1TotalScore = 0, player2TotalScore = 0;
 
+const diceImage = document.querySelector('.dice');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
+const btnPlayer1CurrentScore = document.querySelector('#current--0');
+const btnPlayer2CurrentScore = document.querySelector('#current--1');
+const btnPlayer1TotalScore = document.querySelector('#score--0');
+const btnPlayer2TotalScore = document.querySelector('#score--1');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnNewGame = document.querySelector('.btn--new');
+
 let diceRolled = function() {
+    diceImage.classList.remove('display--none');
     if(player1TotalScore >= 100) {
         alert("Player1 won!!, start the new game.");
     }
@@ -12,50 +24,58 @@ let diceRolled = function() {
     }
     else {
         let randomNumber = Math.floor(Math.random()*6)+1;
-        document.querySelector('.dice').setAttribute('src', './images/dice-'+randomNumber+'.png');
+        diceImage.setAttribute('src', './images/dice-'+randomNumber+'.png');
+        // we can also write upper statement as..
+        // diceImage.src = `./images/dice-${randomNumber}.png`;
         console.log(`random Number is ${randomNumber}`);
     
-        let player1 = document.querySelector('.player--0');
-        let player2 = document.querySelector('.player--1');
         if(player1.classList.contains('player--active')) {
             player1CurrentScore += randomNumber;
-            document.querySelector('#current--0').textContent = player1CurrentScore;
+            btnPlayer1CurrentScore.textContent = player1CurrentScore;
         }
         else {
             player2CurrentScore += randomNumber;
-            document.querySelector('#current--1').textContent = player2CurrentScore;
+            btnPlayer2CurrentScore.textContent = player2CurrentScore;
         }
         if(randomNumber === 1) {
-            if(player1.classList.contains('player--active')) {
-                player1TotalScore += player1CurrentScore;
-                document.querySelector('#score--0').textContent = player1TotalScore;
-                player1CurrentScore = 0;
-                document.querySelector('#current--0').textContent = 0;
-                player1.classList.remove('player--active');
-                player2.classList.add('player--active');
-            }
-            else {
-                player2TotalScore += player2CurrentScore;
-                document.querySelector('#score--1').textContent = player2TotalScore;
-                player2CurrentScore = 0;
-                document.querySelector('#current--1').textContent = 0;
-                player2.classList.remove('player--active');
-                player1.classList.add('player--active');
-            }
+            switchPlayer();
         }
     }
 }
 
-let startNewGame = function() {
-    document.querySelector('#score--0').textContent = 0;
-    document.querySelector('#score--1').textContent = 0;
-    document.querySelector('#current--0').textContent = 0;
-    document.querySelector('#current--1').textContent = 0;
-    document.querySelector('.dice').classList.add('display--none');   
-    player1CurrentScore = player2CurrentScore = 0;
-    player1TotalScore = player2TotalScore = 0; 
+let switchPlayer = function() {
+    if(player1.classList.contains('player--active')) {
+        player1TotalScore += player1CurrentScore;
+        btnPlayer1TotalScore.textContent = player1TotalScore;
+        player1CurrentScore = 0;
+        btnPlayer1CurrentScore.textContent = 0;
+        player1.classList.remove('player--active');
+        player2.classList.add('player--active');
+    }
+    else {
+        player2TotalScore += player2CurrentScore;
+        btnPlayer2TotalScore.textContent = player2TotalScore;
+        player2CurrentScore = 0;
+        btnPlayer2CurrentScore.textContent = 0;
+        player2.classList.remove('player--active');
+        player1.classList.add('player--active');
+    }
 }
 
-document.querySelector('.btn--roll').addEventListener('click', diceRolled);
+let startNewGame = function() {
+    btnPlayer1TotalScore.textContent = 0;
+    btnPlayer2TotalScore.textContent = 0;
+    btnPlayer1CurrentScore.textContent = 0;
+    btnPlayer2CurrentScore.textContent = 0;
+    diceImage.classList.add('display--none');   
+    player1CurrentScore = player2CurrentScore = 0;
+    player1TotalScore = player2TotalScore = 0; 
+    player2.classList.remove('player--active');
+    player1.classList.add('player--active');
+}
 
-document.querySelector('.btn--new').addEventListener('click', startNewGame);
+btnRoll.addEventListener('click', diceRolled);
+
+btnHold.addEventListener('click', switchPlayer);
+
+btnNewGame.addEventListener('click', startNewGame);
